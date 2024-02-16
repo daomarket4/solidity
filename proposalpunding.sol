0f2fb02e9ab7e5ac1640167a7b1a734cf823a0ac
nanamingg <nana172@kakao.com>
2024-02-16 18:57:00 +0900

    add getProposalContributor

--- proposalpunding.sol
+++ proposalpunding.sol
@@ -9,7 +9,7 @@ contract ProposalAndFunding {
         string nftLink; // 구매하고자 하는 NFT의 링크
         string imageLink; // 안건 관련 이미지 링크
         uint256 fundingGoal; // 목표 펀딩 금액
-        uint256 amountRaised; // 현재까지 모금된 금액 
+        uint256 amountRaised; // 현재까지 모금된 금액
         uint256 startTime; // 펀딩 시작 시간
         uint256 endTime; // 펀딩 종료 시간
         string description; // 안건의 설명
@@ -18,13 +18,19 @@ contract ProposalAndFunding {
 
     // 모든 안건을 저장하는 배열
     Proposal[] public proposals;
+
     // 제안 ID와 참여자 주소에 따른 기여 금액을 추적하는 매핑
     mapping(uint256 => mapping(address => uint256)) public contributions;
 
+    // 제안 ID별 기여자 주소 목록
+    mapping(uint256 => address[]) public proposalContributors;
+
     // 새 안건이 생성될 때 발생하는 이벤트
     event NewProposal(uint256 indexed proposalId, address indexed proposer, uint256 fundingGoal, uint256 startTime, uint256 endTime);
+
     // 펀딩에 참여할 때 발생하는 이벤트
     event FundingReceived(uint256 indexed proposalId, address contributor, uint256 amount);
+
     // 펀딩이 종료될 때 발생하는 이벤트
     event FundingClosed(uint256 indexed proposalId, uint256 amountRaised);
 
@@ -41,20 +47,21 @@ contract ProposalAndFunding {
         
         uint256 startTime = block.timestamp;
         uint256 endTime = startTime + (_durationInDays * 1 days);
-        // uint256 fundingGoalWithMargin = _fundingGoal + (_fundingGoal * 10 / 100); 아마삭제할듯? 프론트에서도 구현가능
         
-        proposals.push(Proposal({
+        Proposal memory newProposal = Proposal({
             proposer: msg.sender,
             title: _title,
             nftLink: _nftLink,
             imageLink: _imageLink,
-            fundingGoal: _fundingGoal, //이렇게수정
+            fundingGoal: _fundingGoal,
             amountRaised: 0,
             startTime: startTime,
             endTime: endTime,
             description: _description,
             fundingClosed: false
-        }));
+        });
+        
+        proposals.push(newProposal);
         uint256 proposalId = proposals.length - 1;
         
         emit NewProposal(proposalId, msg.sender, _fundingGoal, startTime, endTime);
@@ -62,13 +69,16 @@ contract ProposalAndFunding {
 
     // 펀딩에 참여하는 함수
     function fundProposal(uint256 _proposalId) public payable {
+        require(_proposalId < proposals.length, "Proposal does not exist");
         Proposal storage proposal = proposals[_proposalId];
+        
         require(!proposal.fundingClosed, "Funding is already closed");
         require(block.timestamp >= proposal.startTime && block.timestamp <= proposal.endTime, "Funding period is not active");
         require(proposal.amountRaised + msg.value <= proposal.fundingGoal, "Funding goal exceeded");
-
+        
         contributions[_proposalId][msg.sender] += msg.value;
         proposal.amountRaised += msg.value;
+        proposalContributors[_proposalId].push(msg.sender);
 
         emit FundingReceived(_proposalId, msg.sender, msg.value);
 
@@ -80,34 +90,37 @@ contract ProposalAndFunding {
 
     // 목표 금액 도달 여부를 확인하는 함수
     function isFundingGoalReached(uint256 _proposalId) public view returns (bool) {
+        require(_proposalId < proposals.length, "Proposal does not exist");
         Proposal storage proposal = proposals[_proposalId];
         return proposal.amountRaised >= proposal.fundingGoal;
     }
 
-     // 펀딩 종료 후 환불 처리 함수
+    // 펀딩 종료 후 환불 처리 함수
     function finalizeAndRefund(uint256 _proposalId) public {
+        require(_proposalId < proposals.length, "Proposal does not exist");
         Proposal storage proposal = proposals[_proposalId];
-        // 펀딩 기간 종료 및 상태 검증
+        
         require(block.timestamp > proposal.endTime, "Funding period is not over yet");
         require(!proposal.fundingClosed, "Funding is already closed");
-
+        
         proposal.fundingClosed = true;
 
-        // 목표 금액에 미달한 경우 환불 처리
         if (proposal.amountRaised < proposal.fundingGoal) {
-            for (uint256 i = 0; i < proposals.length; i++) {
-                uint256 contributedAmount = contributions[_proposalId][proposals[i].proposer];
+            address[] memory contributors = proposalContributors[_proposalId];
+            for (uint256 i = 0; i < contributors.length; i++) {
+                address payable contributor = payable(contributors[i]);
+                uint256 contributedAmount = contributions[_proposalId][contributor];
+                
                 if (contributedAmount > 0) {
-                    address payable contributor = payable(proposals[i].proposer);
-                    contributions[_proposalId][contributor] = 0;
-                    contributor.transfer(contributedAmount);
+                    contributions[_proposalId][contributor] = 0; // 기여 금액 초기화
+                    (bool sent, ) = contributor.call{value: contributedAmount}("");
+                    require(sent, "Failed to send Ether");
                 }
             }
         }
 
         emit FundingClosed(_proposalId, proposal.amountRaised);
     }
-
 
     // 안건 상세 정보를 조회하는 함수
     function getProposal(uint256 _proposalId) public view returns (
@@ -122,6 +135,7 @@ contract ProposalAndFunding {
         string memory description,
         bool fundingClosed
     ) {
+        require(_proposalId < proposals.length, "Proposal does not exist");
         Proposal storage proposal = proposals[_proposalId];
         return (
             proposal.proposer,
@@ -140,6 +154,16 @@ contract ProposalAndFunding {
     function getProposalsCount() public view returns (uint256) { 
         return proposals.length; 
     }
+
+    // 특정 제안에 대한 모든 기여자 주소 목록을 반환하는 함수
+function getProposalContributors(uint256 _proposalId) public view returns (address[] memory) {
+    return proposalContributors[_proposalId];
+}
+
+// 특정 제안과 기여자 주소에 대한 기여 금액을 반환하는 함수
+function getContributionAmount(uint256 _proposalId, address _contributor) public view returns (uint256) {
+    return contributions[_proposalId][_contributor];
+}
 
     // 이더리움을 받을 수 있는 fallback 함수
     receive() external payable {}
